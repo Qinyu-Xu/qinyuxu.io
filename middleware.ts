cfb92318259577863e6b01701abf3605759acb4b
Qinyu Xu <qx774@10-21-91-123.dynapool.wireless.nyu.edu>
2026-03-05 18:24:54 -0500

    fix: add www redirection

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host')?.split(':')[0] ?? '';
+  const hostname = request.headers.get('host') ?? '';
+  const hostnameWithoutPort = hostname.split(':')[0];
 
-  if (hostname === 'qinyuxu.me') {
-    return NextResponse.rewrite(new URL('/me', request.url));
+  console.log('host header:', hostname);
+  console.log('hostname:', hostnameWithoutPort);
+
+  if (hostnameWithoutPort === 'qinyuxu.me' || hostnameWithoutPort === 'www.qinyuxu.me') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/me';
+    return NextResponse.rewrite(url);
   }
 
-  if (hostname === 'qinyuxu.io' && request.nextUrl.pathname === '/me') {
+  if (
+    (hostnameWithoutPort === 'qinyuxu.io' || hostnameWithoutPort === 'www.qinyuxu.io') &&
+    request.nextUrl.pathname === '/me'
+  ) {
     return NextResponse.redirect('https://qinyuxu.me', 302);
   }
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/((?!_next|favicon.ico).*)'],
 };
